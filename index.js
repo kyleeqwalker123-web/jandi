769a964884ad62c1dc9910e72e5b027ae59d9109
kyleeqwalker123-web <kyleeqwalker123@gmail.com>
2026-01-22 00:49:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, { cors: { origin: "*" } });
-const path = require('path');
 
 app.use(express.static(__dirname));
 let messageHistory = {}; 
-let roomMoods = {}; 
 
 io.on('connection', (socket) => {
   socket.on('join room', (room) => {
     socket.join(room);
     if (messageHistory[room]) socket.emit('load history', messageHistory[room]);
-    if (roomMoods[room]) io.to(room).emit('update mood', roomMoods[room]);
   });
 
   socket.on('chat message', (data) => {
@@ -21,13 +18,18 @@ io.on('connection', (socket) => {
     io.to(data.room).emit('chat message', data);
   });
 
-  // MOOD LOGIC
-  socket.on('change mood', (data) => {
-    roomMoods[data.room] = data.mood;
-    io.to(data.room).emit('update mood', data.mood);
+  socket.on('typing', (data) => {
+    socket.to(data.room).emit('user typing', data);
   });
 
-  // BUZZ LOGIC
+  socket.on('play video', (data) => {
+    io.to(data.room).emit('sync video', data.videoId);
+  });
+
+  socket.on('send location', (data) => {
+    socket.to(data.room).emit('receive location', data);
+  });
+
   socket.on('send buzz', (room) => {
     socket.to(room).emit('get buzzed');
   });
