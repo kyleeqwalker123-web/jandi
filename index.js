b28147be9d7b24ca6d1c501575f92140f6285fdd
kyleeqwalker123-web <kyleeqwalker123@gmail.com>
2026-01-22 00:21:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,37 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http, {
-  cors: { origin: "*", methods: ["GET", "POST"] }
-});
+const io = require('socket.io')(http, { cors: { origin: "*" } });
 const path = require('path');
 
 app.use(express.static(__dirname));
-
-// Temporary storage for messages
 let messageHistory = {}; 
+let roomMoods = {}; 
 
 io.on('connection', (socket) => {
   socket.on('join room', (room) => {
     socket.join(room);
-    
-    // Send previous messages to the user who just joined
-    if (messageHistory[room]) {
-      socket.emit('load history', messageHistory[room]);
-    }
+    if (messageHistory[room]) socket.emit('load history', messageHistory[room]);
+    if (roomMoods[room]) io.to(room).emit('update mood', roomMoods[room]);
   });
 
   socket.on('chat message', (data) => {
-    if (!messageHistory[data.room]) {
-      messageHistory[data.room] = [];
-    }
-    // Add new message to memory
+    if (!messageHistory[data.room]) messageHistory[data.room] = [];
     messageHistory[data.room].push(data);
-    
-    // Limit to last 100 messages
-    if (messageHistory[data.room].length > 100) {
-      messageHistory[data.room].shift();
-    }
-
     io.to(data.room).emit('chat message', data);
+  });
+
+  // MOOD LOGIC
+  socket.on('change mood', (data) => {
+    roomMoods[data.room] = data.mood;
+    io.to(data.room).emit('update mood', data.mood);
+  });
+
+  // BUZZ LOGIC
+  socket.on('send buzz', (room) => {
+    socket.to(room).emit('get buzzed');
   });
 });
 
 const PORT = process.env.PORT || 10000;
-http.listen(PORT, '0.0.0.0', () => {
-  console.log('Server running');
-});
+http.listen(PORT, '0.0.0.0', () => { console.log('Server running'); });
