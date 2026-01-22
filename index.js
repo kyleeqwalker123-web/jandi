69df407401f662c2819c14b92bb85a5751d5e16e
kyleeqwalker123-web <kyleeqwalker123@gmail.com>
2026-01-21 23:50:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,26 +8,36 @@ const path = require('path');
 
 app.use(express.static(__dirname));
 
-io.on('connection', (socket) => {
-  console.log('A user connected: ' + socket.id);
+// Temporary storage for messages
+let messageHistory = {}; 
 
+io.on('connection', (socket) => {
   socket.on('join room', (room) => {
     socket.join(room);
-    console.log(`User ${socket.id} joined room: ${room}`);
+    
+    // Send previous messages to the user who just joined
+    if (messageHistory[room]) {
+      socket.emit('load history', messageHistory[room]);
+    }
   });
 
   socket.on('chat message', (data) => {
-    console.log(`Message received for room ${data.room}: ${data.msg}`);
-    // This line sends it to everyone INCLUDING the sender
-    io.to(data.room).emit('chat message', data);
-  });
+    if (!messageHistory[data.room]) {
+      messageHistory[data.room] = [];
+    }
+    // Add new message to memory
+    messageHistory[data.room].push(data);
+    
+    // Limit to last 100 messages
+    if (messageHistory[data.room].length > 100) {
+      messageHistory[data.room].shift();
+    }
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
+    io.to(data.room).emit('chat message', data);
   });
 });
 
 const PORT = process.env.PORT || 10000;
 http.listen(PORT, '0.0.0.0', () => {
-  console.log('Server is running on port ' + PORT);
+  console.log('Server running');
 });
