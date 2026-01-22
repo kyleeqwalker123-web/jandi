fd37a21529906aa4aff71263c88efff955c3e027
kyleeqwalker123-web <kyleeqwalker123@gmail.com>
2026-01-21 23:11:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "*", // This allows your phone and Mac to talk to each other
+    methods: ["GET", "POST"]
+  }
+});
 const path = require('path');
+
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
-  socket.on('join room', (room) => socket.join(room));
-  socket.on('chat message', (data) => io.to(data.room).emit('chat message', data));
-  socket.on('react', (data) => io.to(data.room).emit('react', data));
+  console.log('User connected');
+  
+  socket.on('join room', (room) => {
+    socket.join(room);
+    console.log('User joined room: ' + room);
+  });
+
+  socket.on('chat message', (data) => {
+    // This sends the message to EVERYONE in the room
+    io.to(data.room).emit('chat message', data);
+  });
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log('Server online on port ' + PORT));
+const PORT = process.env.PORT || 10000;
+http.listen(PORT, () => {
+  console.log('Server is running on port ' + PORT);
+});
