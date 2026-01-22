6ad766966ea8681ccf9e00bca8859831a48bf8e6
kyleeqwalker123-web <kyleeqwalker123@gmail.com>
2026-01-22 01:45:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http, { cors: { origin: "*" } });
+const io = require('socket.io')(http, { 
+    cors: { origin: "*" },
+    maxHttpBufferSize: 1e7 // 10MB limit for high-quality images/doodles
+});
 
 app.use(express.static(__dirname));
-
 let roomData = {};
 
 io.on('connection', (socket) => {
   socket.on('join room', (room) => {
     socket.join(room);
-    if (!roomData[room]) {
-      roomData[room] = { messages: [], currentVideo: null };
-    }
+    if (!roomData[room]) roomData[room] = { messages: [] };
     if (roomData[room].messages.length > 0) socket.emit('load history', roomData[room].messages);
-    if (roomData[room].currentVideo) socket.emit('sync video', roomData[room].currentVideo);
   });
 
   socket.on('chat message', (data) => {
     if (roomData[data.room]) {
-      roomData[data.room].messages.push(data);
+        roomData[data.room].messages.push(data);
+        if (roomData[data.room].messages.length > 50) roomData[data.room].messages.shift();
     }
     io.to(data.room).emit('chat message', data);
   });
 
-  socket.on('play video', (data) => {
-    if (roomData[data.room]) roomData[data.room].currentVideo = data;
-    io.to(data.room).emit('sync video', data);
+  // New Interactive Events
+  socket.on('typing', (data) => {
+    socket.to(data.room).emit('typing', data);
   });
 
-  socket.on('send location', (data) => {
-    socket.to(data.room).emit('receive location', data);
+  socket.on('heart_burst', (room) => {
+    io.to(room).emit('heart_burst');
   });
 
-  socket.on('send buzz', (room) => {
-    io.to(room).emit('get buzzed');
-  });
+  socket.on('sync video', (data) => { io.to(data.room).emit('sync video', data); });
+  socket.on('send location', (data) => { socket.to(data.room).emit('receive location', data); });
+  socket.on('send buzz', (room) => { io.to(room).emit('get buzzed'); });
 });
 
 const PORT = process.env.PORT || 10000;
-http.listen(PORT, '0.0.0.0', () => { console.log('Server running'); });
+http.listen(PORT, '0.0.0.0', () => { console.log(`Server running on port ${PORT}`); });
