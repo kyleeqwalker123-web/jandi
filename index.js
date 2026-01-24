23a64a0cbb0d04f5e81051aa165693ea189c6e48
kyleeqwalker123-web <kyleeqwalker123@gmail.com>
2026-01-24 01:21:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,52 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, { cors: { origin: "*" } });
 
 app.use(express.static(__dirname));
-let roomHistory = {};
+let roomData = {};
 
 io.on('connection', (socket) => {
-    socket.on('join', (data) => {
-        socket.join(data.room);
-        if (!roomHistory[data.room]) roomHistory[data.room] = [];
-        socket.emit('load_history', roomHistory[data.room]);
-    });
+    let userRoom = "";
+    let userLabel = "";
 
-    socket.on('chat_msg', (data) => {
-        if (roomHistory[data.room]) {
-            roomHistory[data.room].push(data);
-            if (roomHistory[data.room].length > 50) roomHistory[data.room].shift();
+    socket.on('join room', (data) => {
+        const { room, user } = data;
+        userRoom = room; userLabel = user;
+        socket.join(room);
+        
+        if (!roomData[room]) {
+            roomData[room] = { messages: [], users: {'J':'offline','I':'offline'} };
         }
-        io.to(data.room).emit('chat_msg', data);
+        
+        roomData[room].users[user] = "online";
+        io.to(room).emit('status_update', roomData[room].users);
+        
+        // Send history to the user joining
+        if (roomData[room].messages.length > 0) {
+            socket.emit('load history', roomData[room].messages);
+        }
     });
 
-    socket.on('video_change', (data) => io.to(data.room).emit('video_change', data.id));
-    socket.on('video_action', (data) => socket.to(data.room).emit('video_action', data.action));
-    socket.on('send_reaction', (data) => io.to(data.room).emit('receive_reaction', data.emoji));
-    socket.on('sync_trivia', (data) => io.to(data.room).emit('receive_trivia', data));
-    socket.on('reveal_trivia', (room) => io.to(room).emit('reveal_trivia'));
+    socket.on('chat message', (data) => {
+        if (roomData[data.room]) {
+            roomData[data.room].messages.push(data);
+            if (roomData[data.room].messages.length > 200) roomData[data.room].messages.shift();
+        }
+        io.to(data.room).emit('chat message', data);
+    });
+
+    socket.on('disconnect', () => {
+        if (userRoom && roomData[userRoom]) {
+            roomData[userRoom].users[userLabel] = "offline";
+            io.to(userRoom).emit('status_update', roomData[userRoom].users);
+        }
+    });
+
+    socket.on('typing', (data) => socket.to(data.room).emit('typing', data));
+    socket.on('sync video', (data) => io.to(data.room).emit('sync video', data));
+    socket.on('heart_burst', (room) => io.to(room).emit('heart_burst'));
 });
 
-http.listen(process.env.PORT || 10000, '0.0.0.0');
+const PORT = process.env.PORT || 10000;
+http.listen(PORT, '0.0.0.0', () => console.log('Hub Server Active'));
