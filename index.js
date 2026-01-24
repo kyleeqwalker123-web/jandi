b901dfa132dcf2b064dde3c55e1c667f7f7ce899
kyleeqwalker123-web <kyleeqwalker123@gmail.com>
2026-01-24 01:08:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http, { cors: { origin: "*" }});
+const io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
+let roomHistory = {};
 
 io.on('connection', (socket) => {
-    let currentRoom = "";
-
-    socket.on('join', (room) => {
-        currentRoom = room;
-        socket.join(room);
+    socket.on('join', (data) => {
+        socket.join(data.room);
+        if (!roomHistory[data.room]) roomHistory[data.room] = [];
+        socket.emit('load_history', roomHistory[data.room]);
     });
 
-    // Video Syncing
+    socket.on('chat_msg', (data) => {
+        if (roomHistory[data.room]) {
+            roomHistory[data.room].push(data);
+            if (roomHistory[data.room].length > 50) roomHistory[data.room].shift();
+        }
+        io.to(data.room).emit('chat_msg', data);
+    });
+
     socket.on('video_change', (data) => io.to(data.room).emit('video_change', data.id));
     socket.on('video_action', (data) => socket.to(data.room).emit('video_action', data.action));
-    
-    // Emoji Reactions
     socket.on('send_reaction', (data) => io.to(data.room).emit('receive_reaction', data.emoji));
-
-    // Trivia Syncing
     socket.on('sync_trivia', (data) => io.to(data.room).emit('receive_trivia', data));
     socket.on('reveal_trivia', (room) => io.to(room).emit('reveal_trivia'));
 });
 
-const PORT = process.env.PORT || 10000;
-http.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+http.listen(process.env.PORT || 10000, '0.0.0.0');
